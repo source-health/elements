47e9efc63e46209c070f37e67959469835d007ed
Colin Morelli <colin.morelli@gmail.com>
2022-11-30 10:42:29 -0500

    Changing types for message send

--- src/components/communications/Thread/Thread.tsx
+++ src/components/communications/Thread/Thread.tsx
@@ -1,14 +1,8 @@
-import type {
-  Expandable,
-  File,
-  Message,
-  MessageCreateParams,
-  Thread as ThreadResource,
-} from '@source-health/client'
+import type { Expandable, Message, Thread as ThreadResource } from '@source-health/client'
 import React, { FunctionComponent, useCallback, useEffect, useMemo, useReducer } from 'react'
 
 import { useMember, useSourceClient } from '../../../context/elements'
-import { ThreadContext } from '../../../context/thread'
+import { MessageCreateInputs, ThreadContext } from '../../../context/thread'
 
 import { threadInitialState, threadReducer } from './reducer'
 
@@ -31,7 +25,7 @@ export const Thread: FunctionComponent<ThreadProps> = ({ id, children, onSend })
   const { messages, isLoading, hasMoreMessages } = state
 
   const sendMessage = useCallback(
-    async (params: Omit<MessageCreateParams, 'thread'>) => {
+    async (params: MessageCreateInputs) => {
       if (!member) {
         return
       }
@@ -52,11 +46,11 @@ export const Thread: FunctionComponent<ThreadProps> = ({ id, children, onSend })
         sent_at: new Date().toISOString(),
         attachments:
           params.attachments?.map((attachment) => ({
-            type: attachment.type,
-            description: attachment.description ?? null,
-            url: attachment.url ?? '',
+            type: 'file',
+            description: attachment.file.name,
+            url: attachment.file.url,
             metadata: attachment.metadata ?? {},
-            resource: (attachment.resource ?? null) as Expandable<File> | null,
+            resource: attachment.file,
           })) ?? [],
         impersonated_by: null,
         redacted_at: null,
@@ -70,8 +64,14 @@ export const Thread: FunctionComponent<ThreadProps> = ({ id, children, onSend })
       try {
         const created = await client.communications.messages.create(
           {
-            ...params,
+            text: params.text,
             thread: id,
+            attachments:
+              params.attachments?.map((attachment) => ({
+                type: 'file',
+                resource: attachment.file.id,
+                metadata: attachment.metadata ?? {},
+              })) ?? [],
           },
           {
             expand: ['sender', 'sender.profile_image', 'attachments.resource'],
--- src/context/thread.tsx
+++ src/context/thread.tsx
@@ -1,33 +1,57 @@
-import type { MessageCreateParams, Message } from '@source-health/client'
+import type { File, Message } from '@source-health/client'
 import { createContext, useContext } from 'react'
+
+export interface MessageCreateAttachmentInputs {
+  /**
+   * The file to attach
+   */
+  file: File
+
+  /**
+   * Arbitrary metadata to include on this attachment
+   */
+  metadata?: Record<string, unknown>
+}
+
+export interface MessageCreateInputs {
+  /**
+   * The message content to send
+   */
+  text: string
+
+  /**
+   * Array of attachments to send (as files). Note that the files must already have been uploaded.
+   */
+  attachments?: ReadonlyArray<MessageCreateAttachmentInputs>
+}
 
 export interface ThreadContextValue {
   id: string
 
   /**
-   *
+   * Whether or not the thread is currently being loaded
    */
   isLoading: boolean
 
   /**
-   *
+   * The messages known to exist on the thread (an empty array when isLoading = true)
    */
   messages: Message[]
 
   /**
-   *
+   * Whether or not there are more messages that can be loaded on the thread
    */
   hasMoreMessages: boolean
 
   /**
-   *
+   * Callback to fetch more messages on the thread
    */
   fetchMoreMessages: () => void
 
   /**
-   *
+   * Send a message on the thread
    */
-  sendMessage: (params: Omit<MessageCreateParams, 'thread'>) => Promise<void>
+  sendMessage: (params: MessageCreateInputs) => Promise<void>
 }
 
 export const ThreadContext = createContext<ThreadContextValue | null>(null)
